a40249f85b50c6f6f9027068105dbcdd69646f5a
Gionni Valeriana <joy.lazari@gmail.com>
2015-01-03 00:39:39 +0100

    building test env

---
+++ Tests/Resources/AppKernel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Joy Lazari <joy.lazari@gmail.com>
+ * @date 03/01/15
+ */
+
+use Symfony\Cmf\Component\Testing\HttpKernel\TestKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends TestKernel {
+    public function configure() {
+        $this->requireBundleSets(array(
+            'default',
+        ));
+
+        $this->addBundles(array(
+            new \Symfony\Cmf\Bundle\MyBundle\CmfMyBundle(),
+        ));
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader){
+        $loader->load(__DIR__.'/config/config.php');
+    }
+}
