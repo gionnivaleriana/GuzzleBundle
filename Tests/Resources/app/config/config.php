e5d336660938a3fb7cd441b7d0933f6a6d3aea3a
Gionni Valeriana <joy.lazari@gmail.com>
2015-01-03 01:46:45 +0100

    building test env

---
+++ Tests/Resources/app/config/config.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @author Joy Lazari <joy.lazari@gmail.com>
+ * @date 03/01/15
+ */
+$loader->import(CMF_TEST_CONFIG_DIR.'/default.php');
---
+++ Tests/Resources/app/config/routing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Joy Lazari <joy.lazari@gmail.com>
+ * @date 03/01/15
+ */
+
+use Symfony\Component\Routing\RouteCollection;
+
+$collection = new RouteCollection();
+
+return $collection;
